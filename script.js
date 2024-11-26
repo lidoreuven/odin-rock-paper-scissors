4a6e9dfaddeeee4adb6ace50f4e11215ba1ec9a1
Lidor Reuven <184446343+lidoreuven@users.noreply.github.com>
2024-11-26 16:04:29 +0200

    END STEP 5

--- script.js
+++ script.js
@@ -17,12 +17,42 @@ function getHumanChoice() {
     return prompt("Enter your choice: ");
 }
 
-getHumanChoice();
-
 /* step 4 */
 let humanScore = 0, computerScore = 0;
 
 /*STEP 5*/
 function playRound(humanChoice, computerChoice) {
+    let inCaseHumanChoice = humanChoice.toLowerCase();
+    let massage = "";
+    switch (inCaseHumanChoice) {
+        case "paper":
+            (computerChoice == "rock") ? massage = "win" : (computerChoice == "paper" ? massage = "tie" : massage = "lose");
+            break;
+        case "rock":
+            (computerChoice == "scissors") ? massage = "win" : (computerChoice == "rock" ? massage = "tie" : massage = "lose");
+            break;
+        case "scissors":
+            (computerChoice == "paper") ? massage = "win" : (computerChoice == "scissors" ? massage = "tie" : massage = "lose");
+            break;
+    }
+    if (massage == "tie"){
+        console.log("It's a tie!");
+    } 
+    else if (massage == "win"){
+        console.log(`You ${massage}, ${inCaseHumanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    else {
+        console.log(`You ${massage}, ${computerChoice} beats ${inCaseHumanChoice}`);
+        computerScore++;
+    }
+    
 
-}
+  }
+  
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  
+  console.log(`Computer Choice is: ${computerSelection}`);
+  console.log(`Human Choice is: ${humanSelection}`);
+  playRound(humanSelection, computerSelection);
