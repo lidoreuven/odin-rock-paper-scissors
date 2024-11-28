81da67d70c0ab7a07d942830f90b0074c1b97a95
Lidor Reuven <184446343+lidoreuven@users.noreply.github.com>
2024-11-28 15:11:11 +0200

    Solved the project

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-
-
 /*step 2*/
 function getComputerChoice() {
     let guess = Math.random()*3 + 1;
@@ -16,43 +14,49 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Enter your choice: ");
 }
+  
+
+
+function playGame() {
 
 /* step 4 */
 let humanScore = 0, computerScore = 0;
 
-/*STEP 5*/
-function playRound(humanChoice, computerChoice) {
-    let inCaseHumanChoice = humanChoice.toLowerCase();
-    let massage = "";
-    switch (inCaseHumanChoice) {
-        case "paper":
-            (computerChoice == "rock") ? massage = "win" : (computerChoice == "paper" ? massage = "tie" : massage = "lose");
-            break;
-        case "rock":
-            (computerChoice == "scissors") ? massage = "win" : (computerChoice == "rock" ? massage = "tie" : massage = "lose");
-            break;
-        case "scissors":
-            (computerChoice == "paper") ? massage = "win" : (computerChoice == "scissors" ? massage = "tie" : massage = "lose");
-            break;
-    }
-    if (massage == "tie"){
-        console.log("It's a tie!");
-    } 
-    else if (massage == "win"){
-        console.log(`You ${massage}, ${inCaseHumanChoice} beats ${computerChoice}`);
-        humanScore++;
-    }
-    else {
-        console.log(`You ${massage}, ${computerChoice} beats ${inCaseHumanChoice}`);
-        computerScore++;
-    }
-    
+    /*STEP 5*/
+    function playRound(humanChoice, computerChoice) {
+        let inCaseHumanChoice = humanChoice.toLowerCase();
+        let massage = "";
+        switch (inCaseHumanChoice) {
+            case "paper":
+                (computerChoice == "rock") ? massage = "win" : (computerChoice == "paper" ? massage = "tie" : massage = "lose");
+                break;
+            case "rock":
+                (computerChoice == "scissors") ? massage = "win" : (computerChoice == "rock" ? massage = "tie" : massage = "lose");
+                break;
+            case "scissors":
+                (computerChoice == "paper") ? massage = "win" : (computerChoice == "scissors" ? massage = "tie" : massage = "lose");
+                break;
+        }
+        if (massage == "tie"){
+            console.log("It's a tie!");
+        } 
+        else if (massage == "win"){
+            console.log(`You ${massage}, ${inCaseHumanChoice} beats ${computerChoice}`);
+        }
+        else {
+            console.log(`You ${massage}, ${computerChoice} beats ${inCaseHumanChoice}`);
+        }
+        return massage;
 
-  }
-  
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-  
-  console.log(`Computer Choice is: ${computerSelection}`);
-  console.log(`Human Choice is: ${humanSelection}`);
-  playRound(humanSelection, computerSelection);
+    }    
+
+    for (let i = 0; i<5; i++) {
+        round_massage = playRound(getHumanChoice(), getComputerChoice());
+        if (round_massage = "win") humanScore++;
+        else if  (round_massage = "lose") computerScore++;
+    }
+    console.log(`The Game is over! ${humanScore > computerScore ? "You win!" : humanScore < computerScore ? "You lose!" : "Tie!"}`);
+
+}
+
+playGame();
